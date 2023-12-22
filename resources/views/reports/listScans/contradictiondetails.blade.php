f08a63c5f5cd6d4f939f0b0e125f5526e0210fbc
mohammadreza <mrzhatam@gmail.com>
2023-12-22 13:44:05 -0800

    EDIT REPORT PAGES

--- resources/views/reports/listScans/contradictiondetails.blade.php
+++ resources/views/reports/listScans/contradictiondetails.blade.php
@@ -13,7 +13,96 @@
             <h4 class="py-3 breadcrumb-wrapper mb-4">
                 <span class="text-muted fw-light">جدول‌های داده /</span> پیشرفته
             </h4>
+            <div class="row">
 
+                <!-- Finance Summary -->
+                <div class="col-md-12 col-lg-12 mb-4 order-2 order-lg-0">
+                    <div class="card h-100">
+                        <div class="card-header d-flex align-items-center justify-content-between">
+                            <div class="d-flex align-items-center me-3">
+                                <img src="../../assets/img/avatars/12.png" alt="آواتار" class="rounded-circle me-3"
+                                    width="54">
+                                <div class="card-title mb-0">
+                                    <h5 class="mb-0">گزارش مغایرت در بارنامه  گمرک میرجاوه </h5>
+                                    <small class="text-muted primary-font">جلال صادقی اپراتور اسکنر بهیار 1</small>
+                                </div>
+                            </div>
+                            <div class="dropdown btn-pinned primary-font">
+                                <button class="btn p-0" type="button" id="financoalReport" data-bs-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    <i class="bx bx-dots-vertical-rounded"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="financoalReport">
+                                    <a class="dropdown-item" href="javascript:void(0);">انتخاب همه</a>
+                                    <a class="dropdown-item" href="javascript:void(0);">تازه سازی</a>
+                                    <a class="dropdown-item" href="javascript:void(0);">اشتراک گذاری</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap gap-4 mb-5 mt-4">
+                                <div class="d-flex flex-column me-2">
+                                    <h6>تاریخ شروع</h6>
+                                    <span class="badge bg-label-success">2 آذر 1402</span>
+                                </div>
+                                <div class="d-flex flex-column me-2">
+                                    <h6>تاریخ پایان</h6>
+                                    <span class="badge bg-label-danger">2 آذر 1402</span>
+                                </div>
+                                <div class="d-flex flex-column me-2">
+                                    <h6>اعضا</h6>
+                                    <ul class="list-unstyled me-2 d-flex align-items-center avatar-group mb-0">
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            title="محمدرضا عزیزی" class="avatar avatar-xs pull-up">
+                                            <img class="rounded-circle" src="../../assets/img/avatars/5.png" alt="آواتار">
+                                        </li>
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            title="احسان اسدی" class="avatar avatar-xs pull-up">
+                                            <img class="rounded-circle" src="../../assets/img/avatars/12.png"
+                                                alt="آواتار">
+                                        </li>
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            title="رضا عبدالهی" class="avatar avatar-xs pull-up">
+                                            <img class="rounded-circle" src="../../assets/img/avatars/6.png" alt="آواتار">
+                                        </li>
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            title="جلال صادقی" class="avatar avatar-xs pull-up">
+                                            <img class="rounded-circle" src="../../assets/img/avatars/14.png"
+                                                alt="آواتار">
+                                        </li>
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            title="محسن زمانی" class="avatar avatar-xs pull-up">
+                                            <img class="rounded-circle" src="../../assets/img/avatars/10.png"
+                                                alt="آواتار">
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="d-flex flex-column me-2">
+                                    <h6>نوع مغایرت</h6>
+                                    <span>مغایرت در بار</span>
+                                </div>
+                                <div class="d-flex flex-column me-2">
+                                    <h6>کد بارنامه</h6>
+                                    <span>Br9654</span>
+                                </div>
+                                <div class="d-flex flex-column me-2">
+                                    <h6>نام راننده</h6>
+                                    <span>عباس مقارن عسگری</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        {{-- <div class="card-footer border-top">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item"><i class="bx bx-check"></i> 74 وظیفه</li>
+                                <li class="list-inline-item"><i class="bx bx-chat"></i> 678 دیدگاه</li>
+                            </ul>
+                        </div> --}}
+                    </div>
+                </div>
+                <!-- Finance Summary -->
+
+            </div>
             <!-- Advanced Search -->
             <div class="card">
                 <h5 class="card-header heading-color">جزئیات مغایرت بارنامه BR9654</h5>
--- resources/views/reports/listScans/listcontradiction.blade.php
+++ resources/views/reports/listScans/listcontradiction.blade.php
@@ -2,6 +2,10 @@
 @section('csslink')
     <link rel="stylesheet" href="../../assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css">
     <link rel="stylesheet" href="../../assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css">
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css">
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css">
+    <link rel="stylesheet" href="../../assets/vendor/css/pages/page-profile.css">
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css">
     <link rel="stylesheet" href="../../assets/vendor/libs/flatpickr/flatpickr.css">
 @endsection
 @section('content')
@@ -14,7 +18,42 @@
                 <span class="text-muted fw-light">جدول‌  /</span> ‌ مغایرت بارنامه‌ها
             </h4>
 
+              <!-- Header -->
+              <div class="row">
+                <div class="col-12">
+                  <div class="card mb-4">
+                    <div class="user-profile-header-banner">
+                      <img src="../../assets/img/pages/mirjave.png" alt="Banner image" class="rounded-top">
+                    </div>
+                    <div class="user-profile-header d-flex flex-column flex-sm-row text-sm-start text-center mb-4">
+                      <div class="flex-shrink-0 mt-n2 mx-sm-0 mx-auto">
+                        <img src="../../assets/img/sascan/behyar1.png" alt="user image" class="d-block h-auto ms-0 ms-sm-4 rounded-3 user-profile-img">
+                      </div>
+
+                      <div class="flex-grow-1 mt-3 mt-sm-5">
+                        <div class="d-flex align-items-md-end align-items-sm-start align-items-center justify-content-md-between justify-content-start mx-4 flex-md-row flex-column gap-4">
+                          <div class="user-profile-info">
+                            <h4>پایانه مرزی گمرک میرجاوه</h4>
+                            <ul class="list-inline mb-0 d-flex align-items-center flex-wrap justify-content-sm-start justify-content-center gap-2">
+                              <li class="list-inline-item fw-semibold"><i class="bx bx-pen"></i>دستگاه بهیار 1 </li>
+                              <li class="list-inline-item fw-semibold"><i class="bx bx-map"></i>استان سیستان و بلوچستان</li>
+                              <li class="list-inline-item fw-semibold">
+                                <i class="bx bx-calendar-alt"></i> شروع فعالیت با اسکنر 1399/08/01
+                              </li>
+                            </ul>
+                          </div>
+                          <a href="javascript:void(0)" class="btn btn-primary text-nowrap">
+                            <i class="bx bx-user-check me-1"></i>متصل
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--/ Header -->
             <!-- Advanced Search -->
+
             <div class="card">
                 <h5 class="card-header heading-color">لیست مغایرت‌های ثبت شده</h5>
 
@@ -58,6 +97,7 @@
     <!-- Content wrapper -->
 @endsection
 @section('scripts')
+
     <script src="../../assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
     <script src="../../assets/vendor/libs/datatables-bs5/i18n/fa.js"></script>
     <!-- Flat Picker -->
