ff5f745cb42211b53aac13a1dc895b8672370626
hatammri <mrzhatam@gmail.com>
2024-10-07 23:35:15 +0330

    last changes

--- database/migrations/2014_10_12_000000_create_cities_table.php
+++ database/migrations/2014_10_12_000000_create_cities_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('province_id');
             $table->string('name')->unique();
