e80189c01090990266c2f0d08f2e60c914c55afa
hatammri <mrzhatam@gmail.com>
2024-10-07 23:56:04 +0330

    Update 2014_10_12_000000_create_driver.php

--- database/migrations/2014_10_12_000000_create_driver.php
+++ database/migrations/2014_10_12_000000_create_driver.php
@@ -12,20 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('driver', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('city_id');
+            $table->foreign('city_id')->references('id')->on('city');
             $table->string('name');
-            $table->string('cellphone')->unique();
+            $table->string('phone_number')->unique();
+            $table->string('national_code');
+            $table->string('nationality');
+            $table->string('religion');
+            $table->string('driver_license_number');
+            $table->string('address');
+            $table->string('description');
+            $table->string('is_active');
+            $table->string('criminal_background');
             $table->string('avatar')->default('defult.png');
-            $table->integer('status')->default(1);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('provider_name')->nullable();
-            $table->string('login_token')->nullable();
-            $table->integer('otp')->nullable();
-            $table->string('role')->nullable();
-            $table->string('position')->nullable();
+            $table->string('slug');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
