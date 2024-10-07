37f67c72c2b6c04930036fe2edf7cc319c12051b
hatammri <mrzhatam@gmail.com>
2024-10-08 00:05:43 +0330

    Update 2014_10_12_000000_create_load.php

--- database/migrations/2014_10_12_000000_create_load.php
+++ database/migrations/2014_10_12_000000_create_load.php
@@ -12,20 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('load', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('cellphone')->unique();
-            $table->string('avatar')->default('defult.png');
-            $table->integer('status')->default(1);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('provider_name')->nullable();
-            $table->string('login_token')->nullable();
-            $table->integer('otp')->nullable();
-            $table->string('role')->nullable();
-            $table->string('position')->nullable();
+            $table->foreignId('ownership_id');
+            $table->foreign('ownership_id')->references('id')->on('ownership');
+            $table->string('is_document');
+            $table->string('is_active');
+            $table->timestamp(column: 'created_at')->nullable();
+            $table->timestamp(column: 'updated_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
